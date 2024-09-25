d6f7191a060822adf47f1627c9239132ea2e33ef
mykola <nikolajtarasenko71@gmail.com>
2024-09-25 11:45:03 +0300

    Add TaskType, Project & Task models

--- tasks/models.py
+++ tasks/models.py
@@ -43,3 +43,52 @@ class Worker(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    teams = models.ManyToManyField(Team, related_name="projects")
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN PROGRESS', 'In progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+    PRIORITY_CHOICES = [
+        ('URGENT', 'Urgent'),
+        ('HIGH', 'High'),
+        ('MEDIUM', 'Medium'),
+        ('LOW', 'Low')
+    ]
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
