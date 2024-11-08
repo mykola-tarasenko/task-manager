1da6a242ef5f9e459a82fcb703d38720e8bb6092
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-11-08 22:50:35 +0200

    Add models to models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,106 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="workers"
+    )
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="members"
+    )
+
+    class Meta:
+        ordering = ("username",)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    teams = models.ManyToManyField(Team, related_name="projects")
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN PROGRESS', 'In progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+    PRIORITY_CHOICES = [
+        ('URGENT', 'Urgent'),
+        ('HIGH', 'High'),
+        ('MEDIUM', 'Medium'),
+        ('LOW', 'Low')
+    ]
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='PENDING'
+    )
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
