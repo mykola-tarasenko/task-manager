94a93d5e935510adbbca962bebf736e206de9189
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-11-13 18:17:48 +0200

    Add team list to urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
-from .views import index, PositionListView
+from .views import index, PositionListView, TeamListView
 
 from django.urls import path
 
 urlpatterns = [
     path("", index, name="index"),
-    path("positions/", PositionListView.as_view(), name="position-list")
+    path("positions/", PositionListView.as_view(), name="position-list"),
+    path("teams/", TeamListView.as_view(), name="team-list"),
 ]
 
 app_name = "tasks"
