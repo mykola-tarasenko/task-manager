68028e31ba401da9b39d4605a27a77e8cd4e04e8
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-11-13 17:13:24 +0200

    Add position list route to tasks/urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
-from .views import index
+from .views import index, PositionListView
 
 from django.urls import path
 
 urlpatterns = [
     path("", index, name="index"),
+    path("positions/", PositionListView.as_view(), name="position-list")
 ]
 
 app_name = "tasks"
