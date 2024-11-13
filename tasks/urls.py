ca835d775f8189e5e613663d79d6b8870d113891
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-11-13 18:42:11 +0200

    Add worker list url

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,17 @@
-from .views import index, PositionListView, TeamListView
-
 from django.urls import path
+
+from .views import (
+    index,
+    PositionListView,
+    TeamListView,
+    WorkerListView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("positions/", PositionListView.as_view(), name="position-list"),
     path("teams/", TeamListView.as_view(), name="team-list"),
+    path("workers/", WorkerListView.as_view(), name="worker-list")
 ]
 
 app_name = "tasks"
