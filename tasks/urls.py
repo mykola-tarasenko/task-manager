7cda8ddaf67bc3ce7301ed759197b63f89b9c30e
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-11-14 15:51:17 +0200

    Add project list url

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,14 +4,15 @@ from .views import (
     index,
     PositionListView,
     TeamListView,
-    WorkerListView,
+    WorkerListView, ProjectListView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("positions/", PositionListView.as_view(), name="position-list"),
     path("teams/", TeamListView.as_view(), name="team-list"),
-    path("workers/", WorkerListView.as_view(), name="worker-list")
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("projects/", ProjectListView.as_view(), name="project-list"),
 ]
 
 app_name = "tasks"
