c1046e70249006e9cc2048cddb458aa251bb8767
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-11-28 14:29:37 +0200

    Update Task model

--- tasks/models.py
+++ tasks/models.py
@@ -68,39 +68,39 @@ class TaskType(models.Model):
 
 
 class Task(models.Model):
-    STATUS_CHOICES = [
-        ('PENDING', 'Pending'),
-        ('IN PROGRESS', 'In progress'),
-        ('COMPLETED', 'Completed'),
-    ]
-    PRIORITY_CHOICES = [
-        ('URGENT', 'Urgent'),
-        ('HIGH', 'High'),
-        ('MEDIUM', 'Medium'),
-        ('LOW', 'Low')
-    ]
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    deadline = models.DateField()
-    status = models.CharField(
-        max_length=20,
-        choices=STATUS_CHOICES,
-        default='PENDING'
+    priority = models.IntegerField(
+        choices=[
+            (1, "Urgent"),
+            (2, "High"),
+            (3, "Medium"),
+            (4, "Low"),
+        ],
+        default=4,
     )
-    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    status = models.IntegerField(
+        choices=[
+        (1, "Pending"),
+        (2, "In progress"),
+        (3, "Completed"),
+        ],
+        default=1,
+    )
+    deadline = models.DateField(null=True, blank=True)
     task_type = models.ForeignKey(
         TaskType,
         on_delete=models.CASCADE,
-        related_name="tasks"
+        related_name="tasks",
     )
     project = models.ForeignKey(
         Project,
         on_delete=models.CASCADE,
-        related_name="tasks"
+        related_name="tasks",
     )
+    description = models.TextField(null=True, blank=True)
 
     class Meta:
-        ordering = ("name",)
+        ordering = ("name", "priority", "status", "deadline", "task_type", "project",)
 
     def __str__(self):
         return self.name
