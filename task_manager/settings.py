cfe6c5195ed597733d341cd05804b98503961867
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-01-22 16:41:10 +0200

    Remove DDT

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "tasks",
     "crispy_forms",
     "crispy_bootstrap4",
@@ -49,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
