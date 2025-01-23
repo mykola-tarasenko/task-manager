887a741248f7ff70330f847ab61cb916bf2cc5c5
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-01-23 11:46:47 +0200

    Hide secret key

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 from pathlib import Path
 
@@ -23,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qp(jwkt1tshzr&4x#-5mk86@6=2onywydpjoh0eirastr&d^hn"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
